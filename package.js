6a5cb78f085cb4d4bf227d30c506871b81e43e0a
Tarang Patel <tarangp@me.com>
2014-08-08 11:37:23 +0200

    Updates for 0.9

--- package.js
+++ package.js
@@ -1,8 +1,11 @@
 Package.describe({
-    summary: "Select2 is a jQuery based replacement for select boxes."
+    summary: "Select2 with spiderable compatiblity (no diacritics/accents)",
+    version: "1.0.0"
 });
 
 Package.on_use(function (api) {
+    if(api.versionsFrom) api.versionsFrom("METEOR-CORE@0.9.0-atm");
+
     api.use("jquery", "client");
 
     api.add_files([
